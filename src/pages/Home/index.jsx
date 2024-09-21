1b36bf616c46f82d285dc8ba60a471cced07e3a9
Sulaimon Taofik <Sulaimontaofik08@gmail.com>
2024-09-21 23:50:46 +0100

    Added my cv to the home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,6 +8,8 @@ import { animateUseEffect } from '../../utils.js/animateUseEffect';
 
 export default function Home() {
     const [isVisible, setIsVisible] = useState(false);
+    const [showPreview, setShowPreview] = useState(false);
+    const Taofik_CV = '/TaofikCV.pdf';
 
     
 
@@ -16,6 +18,10 @@ const navigate = useNavigate();
    const  handleNavigateToContact = () => {
     navigate('/contact')
    }
+
+   const handleClick = () => {
+    setShowPreview(true);
+  };
 
  animateUseEffect();
 
@@ -31,7 +37,32 @@ const navigate = useNavigate();
       
       <div  className={`home_contact-me ${isVisible ? 'show' : ''}`}>
         <button onClick={handleNavigateToContact} className='home_contact_button'>Hire Me</button>
-        {/* <a href="" download="Taofik CV"><button className='home_contact_button'>Download CV</button> </a>*/}
+
+
+        {/* Initial button to show preview */}
+      {!showPreview && (
+        <button className='home_contact_button1' onClick={handleClick}>
+          Preview Resume
+        </button>
+      )}
+
+      {/* Show preview and download button after preview is clicked */}
+      {showPreview && (
+        <div>
+          {/* PDF preview */}
+          <iframe 
+            src={Taofik_CV} 
+            style={{ width: '100%', height: '400px', marginTop: '20px' }} 
+            title="CV Preview"
+          ></iframe>
+          <br />
+          {/* Download button */}
+          <a href={Taofik_CV} download="TaofikCV.pdf">
+            <button className='home_contact_button2' disabled>My Resume</button>
+          </a>
+        </div>
+      )}
+        {/* <a href={Taofik_CV} download="TaofikCV.pdf"><button className='home_contact_button'>My Resume</button> </a> */}
       </div>
     </section>
   )
